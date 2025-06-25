36ca39345cfdbfecccfa38fe46c3443746116750
felipesanchez-dev <jfelipe9.121@gmail.com>
2025-06-24 21:56:30 -0500

    Primera Sesion

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import { Server } from "./presentation/server";
+import { envs } from "./config/envs";
+
+(() => {
+  main();
+})();
+
+async function main() {
+  new Server({
+    port: envs.PORT,
+  }).start();
+}
+
+
+
+//expresss 
+
+// Redux, Zustang, fect, GET, useEffect,
